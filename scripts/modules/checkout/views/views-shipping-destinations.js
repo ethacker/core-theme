47661d2e440885206b1565d64c9866079371b603
Aaron Jones <Amjcraft@gmail.com>
2017-08-25 15:14:25 -0500

    fixed issues for setting Multi ship

--- scripts/modules/checkout/views/views-shipping-destinations.js
+++ scripts/modules/checkout/views/views-shipping-destinations.js
@@ -152,7 +152,8 @@ define(["modules/jquery-mozu",
                 "change [data-mz-single-fulfillment-contact]": "handleChangeSingleAddress"
             },
             isMultiShipMode: function(){
-                this.model.isMultiShipMode();
+                (this.model.isMultiShipMode()) ? this.model.set('isMultiShipMode', true) : this.model.set('isMultiShipMode', false);
+                return this.model.get('isMultiShipMode');
             },
             handleChangeSingleAddress: function(e){
                 var self = this;
@@ -203,9 +204,13 @@ define(["modules/jquery-mozu",
             },
             render: function(){
                 var self = this;
+
+                self.isMultiShipMode();
+
                 this.$el.removeClass('is-new is-incomplete is-complete is-invalid').addClass('is-' + this.model.stepStatus());
                 EditableView.prototype.render.apply(this, arguments);
                 this.resize();
+
 
                 $.each(this.$el.find('[data-mz-shipping-destinations-item]'), function(index, val) {
                     var id = $(this).data('mzItemId');
